7aee6f54a37d3e31635a500c448341c3075e4b4f
mihirada <mihiindrajith@gmail.com>
2024-07-22 22:27:01 +0530

    completed sigup and login

--- safetynet_mobile/lib/main.dart
+++ safetynet_mobile/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
-import 'package:safetynet_mobile/drivers/authentication/login_screen.dart';
-import 'package:safetynet_mobile/drivers/authentication/signup_screen.dart';
 import 'package:device_preview/device_preview.dart';
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+
+import 'package:safetynet_mobile/drivers/authentication/login_screen.dart';
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(DevicePreview(
     enabled: true,
     builder: (context) => MyApp(),
@@ -15,22 +17,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'SafetyNET',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primaryColor: Color(0xfffbbe00),
         useMaterial3: true,
       ),
-      home: FutureBuilder(future: null, builder: (context, dataSnapshot) { 
-     return LoginScreen(); 
-          } ,
-        ),
+      home: LoginScreen(),
     );
   }
 }
-
