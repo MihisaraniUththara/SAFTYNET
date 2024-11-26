cc4f54f3a4d668a7e142e9315db5eab34b09e2c3
mihirada <mihiindrajith@gmail.com>
2024-11-26 20:16:23 +0530

    start ride page

--- safetynet_mobile/lib/drivers/pages/start_ride.dart
+++ safetynet_mobile/lib/drivers/pages/start_ride.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:get/get.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:safetynet_mobile/drivers/pages/home.dart';
-import 'package:url_launcher/url_launcher.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: StartRidePage(),
-    );
-  }
-}
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class StartRidePage extends StatefulWidget {
   @override
@@ -24,113 +9,83 @@ class StartRidePage extends StatefulWidget {
 }
 
 class _StartRidePageState extends State<StartRidePage> {
-  static const LatLng _center = LatLng(6.9271, 79.8612); // Colombo Coordinates
+  GoogleMapController? _mapController;
+  Position? _currentPosition;
+  AudioPlayer audioPlayer = AudioPlayer();
+  bool alertPlayed = false;
+
+  final List<LatLng> accidentProneAreas = [
+    LatLng(6.8433, 80.0032),
+    LatLng(6.844, 80.0024),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  // Method to get continuous location updates
+  Future<void> _getCurrentLocation() async {
+    Geolocator.getPositionStream(
+      locationSettings: LocationSettings(accuracy: LocationAccuracy.high),
+    ).listen((Position position) {
+      setState(() {
+        _currentPosition = position;
+      });
+      _checkProximityToDangerZone();
+    });
+  }
+
+  // Check proximity to accident-prone areas
+  void _checkProximityToDangerZone() {
+    if (_currentPosition != null) {
+      for (var dangerZone in accidentProneAreas) {
+        double distance = Geolocator.distanceBetween(
+          _currentPosition!.latitude,
+          _currentPosition!.longitude,
+          dangerZone.latitude,
+          dangerZone.longitude,
+        );
+
+        if (distance < 500 && !alertPlayed) { // 500 meters radius
+          _playAlertSound();
+          setState(() {
+            alertPlayed = true;
+          });
+        } else if (distance >= 500) {
+          setState(() {
+            alertPlayed = false;
+          });
+        }
+      }
+    }
+  }
+
+  // Play sound alert
+  Future<void> _playAlertSound() async {
+    await audioPlayer.play(AssetSource('beep.wav'));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFFfbbe00),
-        title: Text('Map View', style: TextStyle(color: Colors.black)),
+        title: Text("Driver Ride"),
       ),
-      body: Stack(
-        children: <Widget>[
-          FlutterMap(
-            options: MapOptions(
-              initialCenter: _center,
-              initialZoom: 15.0,
+      body: _currentPosition == null
+          ? Center(child: CircularProgressIndicator())
+          : GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: LatLng(_currentPosition!.latitude, _currentPosition!.longitude),
+                zoom: 14.0,
+              ),
+              onMapCreated: (GoogleMapController controller) {
+                _mapController = controller;
+              },
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
             ),
-            children: [
-              TileLayer(
-                urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                subdomains: ['a', 'b', 'c'],
-                userAgentPackageName: 'com.example.app',
-              ),
-              MarkerLayer(
-                markers: [
-                  // Marker(
-                  //   point: _center,
-                  //   builder: (ctx) => Icon(
-                  //     Icons.location_on,
-                  //     color: Colors.red,
-                  //     size: 40.0,
-                  //   ),
-                  // ),
-                ],
-              ),
-              RichAttributionWidget(
-                attributions: [
-                  TextSourceAttribution(
-                    'OpenStreetMap contributors',
-                    onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Positioned(
-            top: 16,
-            left: 16,
-            right: 16,
-            child: Container(
-              padding: EdgeInsets.all(8.0),
-              color: Colors.white,
-              child: Text(
-                'Accident prone area is 300m ahead',
-                style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 16,
-            left: 16,
-            right: 16,
-            child: Center(
-              child: FloatingActionButton(
-                onPressed: () { _showConfirmationDialog(context);},
-                backgroundColor: Colors.red,
-                child: Text(
-                  'STOP',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
-
-  void _showConfirmationDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Confirm'),
-          content: Text('Are you sure you want to stop?'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(); // Dismiss the dialog
-              },
-              child: Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                // Add your stop logic here
-                Navigator.of(context).pop(); 
-                 Navigator.of(context).pop(); 
-              },
-              child: Text('Yes'),
-            ),
-          ],
-        );
-      },
-    );
-  }
