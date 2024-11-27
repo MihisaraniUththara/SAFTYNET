50cf45c2af550212bc5a9d7643b677d6bd9a977f
Mihisarani <mihisaraniuththara@gmail.com>
2024-11-27 10:31:13 +0530

    tables

--- Web/src/Components/StationWise/AccidentProgress.jsx
+++ Web/src/Components/StationWise/AccidentProgress.jsx
@@ -41,29 +41,29 @@ const AccidentData = [
 const AccidentProgress = () => {
   return (
     
-    <div className='bg-white px-4 pb-4 rounded-sm border border-gray-200 text-black w-full'>
-        <strong>
+    <div className='bg-white px-4 pb-4 py-4rounded-sm border border-gray-200 text-black w-full'>
+        <strong><h1><center>
           Recent Accidents
-        </strong>
-        <div className='mt-3'>
+          </center></h1></strong>
+        <div className='mt-3 p-3'>
           <table className='w-full table-auto'>
-            <thead className='font-semibold'>
+            <thead className='bg-gray-100 border-gray-400 font-semibold'>
               <tr>
-                <td>Date</td>
-                <td>Accident Id</td>
-                <td>Incharge Officer</td>
-                <td>Status</td>
-                <td>Option</td>
+                <th className='p-3 tracking-wide'>Date</th>
+                <th className='p-3 tracking-wide'>Accident Id</th>
+                <th className='p-3 tracking-wide'>Incharge Officer</th>
+                <th className='p-3 tracking-wide'>Status</th>
+                <th className='p-3 tracking-wide'>Option</th>
               </tr>
             </thead>
             <tbody>
               {AccidentData.map((Accident)=>(
               <tr key={Accident.AccidentId}>
-                <td>{Accident.date}</td>
-                <td>{Accident.AccidentId}</td>
-                <td>{Accident.InchargeOfficer}</td>
-                <td>{Accident.Status}</td>
-                <td><button>Details</button></td>
+                <td className='text-center p-3'>{Accident.date}</td>
+                <td className='text-center p-3'>{Accident.AccidentId}</td>
+                <td className='text-center p-3'>{Accident.InchargeOfficer}</td>
+                <td className='text-center p-3'>{Accident.Status}</td>
+                <td className='text-center p-3'><button className='bg-yellow-button hover:bg-yellow text-black font-bold py-2 px-4 rounded'>Details</button></td>
               </tr>
               ))}
             </tbody>
