9396a2c84aaaa2beb75e09ac6fce3c7e28fb176a
Dewranga Bandara <126384851+Dewranga-Bandara@users.noreply.github.com>
2024-12-03 10:59:23 +0530

    Update DriversPage.jsx

--- Web/src/Pages/Admin1/DriversPage.jsx
+++ Web/src/Pages/Admin1/DriversPage.jsx
@@ -23,6 +23,7 @@ import { useEffect, useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import { collection, getDocs, updateDoc, deleteDoc, doc } from 'firebase/firestore';
 import { db } from '../../firebase';
+import SideBarAdmin from '../../Components/SideBarAdmin';
 
 const Drivers = () => {
   const [drivers, setDrivers] = useState([]);
@@ -128,11 +129,11 @@ const Drivers = () => {
   };
 
   return (
-    <div className="flex h-screen">
-      <SidebarHeader />
+    <div className="flex h-screen w-screen bg-neutral-100">
+      <SideBarAdmin />
       <div className="flex-1 flex flex-col overflow-hidden mt-16">
         <header className="text-center">
-          <h1 className="text-3xl text-center p-2 font-bold">Registered Drivers</h1>
+          <h1 className="text-3xl text-black text-center p-2 font-bold">Registered Drivers</h1>
         </header>
         <main className="flex-1 overflow-auto p-4">
           <div className="flex justify-end mb-2">
